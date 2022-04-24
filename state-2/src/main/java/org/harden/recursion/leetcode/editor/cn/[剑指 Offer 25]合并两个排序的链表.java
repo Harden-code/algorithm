6449a9c3ed333b1c36deec51fd4eed38e7195da0
harden <1400040636@qq.com>
2022-04-24 14:00:57 +0800

    state-4-1-update

--- state-2/src/main/java/org/harden/recursion/leetcode/editor/cn/[剑指 Offer 25]合并两个排序的链表.java
+++ state-2/src/main/java/org/harden/recursion/leetcode/editor/cn/[剑指 Offer 25]合并两个排序的链表.java
@@ -16,28 +16,75 @@ package org.harden.recursion.leetcode.editor.cn;
 
 import org.harden.link.leetcode.editor.cn.ListNode;
 
+import java.util.List;
+
 /**
- * @author  junsenfu
+ * @author junsenfu
  * @date 2022-04-23 23:16:28
  */
-class HeBingLiangGePaiXuDeLianBiaoLcof{
+class HeBingLiangGePaiXuDeLianBiaoLcof {
     public static void main(String[] args) {
         Solution solution = new HeBingLiangGePaiXuDeLianBiaoLcof().new Solution();
-   }
-//leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+        ListNode node1 = new ListNode(4);
+        ListNode node2 = new ListNode(2, node1);
+        ListNode node3 = new ListNode(1, node2);
+
+        ListNode node1_ = new ListNode(4);
+        ListNode node2_ = new ListNode(3, node1_);
+        ListNode node3_ = new ListNode(1, node2_);
+
+        final ListNode listNode = solution.mergeTwoLists(node3, node3_);
+        System.out.println();
     }
-}
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        // 输入：1->2->4, 1->3->4
+        //输出：1->1->2->3->4->4
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode head = new ListNode();
+            ListNode tail = head;
+            mergeTwoLists_r(l1, l2, tail);
+            return head.next;
+        }
+
+        public void mergeTwoLists_r(ListNode l1, ListNode l2, ListNode tail) {
+            if (l1 == null) {
+                tail.next = l2;
+                return;
+
+            }
+            if (l2 == null) {
+                tail.next = l1;
+                return;
+            }
+            int i1 = l1.val;
+            int i2 = l2.val;
+            if (i1 < i2) {
+                //l1 移动
+                ListNode t1 = l1.next;
+                //断开连接
+                l1.next = null;
+                tail.next = l1;
+                tail = tail.next;
+                mergeTwoLists_r(t1, l2, tail);
+            } else {
+                ListNode t2 = l2.next;
+                l2.next = null;
+                tail.next = l2;
+                tail = tail.next;
+                mergeTwoLists_r(l1, t2, tail);
+            }
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
